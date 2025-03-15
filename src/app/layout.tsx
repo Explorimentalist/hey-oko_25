603d917c6e7b61a8692c4e1b47dd013b9da03bcc
Explorimentalist <73285549+Explorimentalist@users.noreply.github.com>
2025-03-15 21:40:50 +0000

    Add favicon and update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,14 @@ import { ScrollRestoration } from "@/components/shared/ScrollRestoration";
 export const metadata: Metadata = {
   title: "Hey-Oko | Design Portfolio",
   description: "The pure unadulterated design portfolio of Ngatye Brian Oko",
+  icons: {
+    icon: [
+      { url: '/favicon.svg', media: '(prefers-color-scheme: light)' },
+      { url: '/favicon-dark.svg', media: '(prefers-color-scheme: dark)' }
+    ],
+    shortcut: '/favicon.svg',
+    apple: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({
